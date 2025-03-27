5dbdfb89cd81079dc288eb71f558a9a3b03f393f
Nathan Vitor <nathanvitor38@gmail.com>
2025-03-26 21:13:17 -0300

    First Commit

---
+++ Day 1/task/task.py
@@ -0,0 +1,4 @@
+print("Seja Bem Vindo(a) ao Gerador de Nome de Bandas")
+city = input("Em que cidade você nasceu?\n")
+pet = input("Qual o nome do seu pet?\n")
+print("Então o nome da sua banda poderia ser: " "'" + city + " " + pet + "'")
