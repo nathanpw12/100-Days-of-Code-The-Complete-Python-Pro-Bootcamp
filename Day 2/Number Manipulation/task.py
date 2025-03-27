98ff119a457960b5d25ae3927b24ff388e2660ef
Nathan Vitor <nathanvitor38@gmail.com>
2025-03-27 20:52:52 -0300

    Day 2 - Part Final

--- Day 2/Number Manipulation/task.py
+++ Day 2/Number Manipulation/task.py
@@ -1,1 +1,47 @@
 bmi = 84 / 1.65 ** 2
+print(bmi)
+
+print(int(bmi)) # 30
+
+print(round(bmi)) # 31
+
+print(round(bmi, 2)) # 30.85
+
+###### Operadores de Atribuição ######
+
+# Atribui a operação da direita ao número da esquerda
+
+# +=
+#
+# -=
+#
+# *=
+#
+# /=
+
+pontos = 0
+
+pontos += 1 # pontos + 1 = 1
+print(pontos)
+
+pontos -= 1 # pontos - 1 = 0
+print(pontos)
+
+pontos *= 1 # pontos * 1 = 0
+print(pontos)
+
+pontos /= 1 # pontos / 1 = 0.0
+print(pontos)
+
+
+###### f-strings ######
+# Permite usar vários tipos de dados em uma string ao declarar a string com um f logo atrás
+# das aspas
+
+score = 0
+height = 1.8
+is_winning = True
+
+print(f"Your score is = {score}, your height is {height}. You are winning is {is_winning}")
+
+# PEMDAS
--- Day 2/Tip Calculator Project/task.py
+++ Day 2/Tip Calculator Project/task.py
@@ -1,6 +1,7 @@
-print("Welcome to the tip calculator!")
+print("Welcome to the tip calculator! ")
 bill = float(input("What was the total bill? $"))
-tip = int(input("What percentage tip would you like to give? 10 12 15 "))
+tip = int(input("What percentage tip would you like to give? (type just numbers) "))
 people = int(input("How many people to split the bill? "))
-
+total = ((bill / people) * (tip / 100 + 1) )
+print(f"Each person should pay: ${round(total, 2)} ")
 
